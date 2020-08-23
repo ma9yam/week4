2e672d0a1e1be9df3ad5a21beed5fa77b9685e09
ma9yam <ma9yam@gmail.com>
2020-08-24 00:39:53 +1000

    completed 100 percent

--- Lab Week 4 - Activity/Assets/PrintAndHide.cs
+++ Lab Week 4 - Activity/Assets/PrintAndHide.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;
-    int i = 0;
+    public int i;
+    int randomNumber;
     // Start is called before the first frame update
     void Start()
     {
+        i = 0;
+        randomNumber = Random.Range(200, 250);
+   
         
+            
     }
 
     // Update is called once per frame
@@ -17,5 +22,12 @@ public class PrintAndHide : MonoBehaviour
     {
         Debug.Log(gameObject.name + ":" + i);
         i++;
+
+        if (gameObject.tag =="red" && i ==100)
+        {
+            gameObject.SetActive(false);
+           }
+        if (gameObject.tag == "blue" && i == randomNumber)
+            rend.enabled = false;
     }
 }
